15ce43a96ed5c9183bda4906b8f7f319577c1983
Mihail <xp_92@mail.ru>
2021-08-26 20:04:27 +0500

    added eslint, prettier, app settings, db setiings

---
+++ src/db/connect.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import config from "config";
+import log from "../logger";
+
+async function connect() {
+  const databaseUri = config.get("dbUri") as string;
+  try {
+    await mongoose.connect(databaseUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    log.info("Database connected");
+  } catch (error) {
+    log.error("db error", error);
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1);
+  }
+}
+
+export default connect;
