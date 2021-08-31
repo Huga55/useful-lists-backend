986267ed79e69dd03b2962d6afe87ee968d4b92f
Mihail <xp_92@mail.ru>
2021-08-31 23:15:52 +0500

    finish auth test, check all

--- jest/auth.test.ts
+++ jest/auth.test.ts
@@ -1,7 +1,11 @@
 import request from "supertest";
 import app from "../src/app";
 
-describe.skip("Register Part", () => {
+let accessToken = "";
+let refreshToken = "";
+
+describe.skip("Register", () => {
+  const endpoint = "/api/auth/register";
   const data = {
     name: "test name",
     email: "email@mail.ru",
@@ -9,7 +13,7 @@ describe.skip("Register Part", () => {
   };
   it("POST /auth/register -> create new user, tokens in response", async () => {
     const response = await request(app)
-      .post("/api/auth/register")
+      .post(endpoint)
       .send(data)
       .expect("Content-Type", /json/)
       .expect(201);
@@ -22,7 +26,7 @@ describe.skip("Register Part", () => {
   });
   it("POST /auth/register -> 409 error, user with this email is already exist", async () => {
     const response = await request(app)
-      .post("/api/auth/register")
+      .post(endpoint)
       .send(data)
       .expect("Content-Type", /json/)
       .expect(409);
@@ -32,9 +36,9 @@ describe.skip("Register Part", () => {
       })
     );
   });
-  it("POST /auth/register -> 400 error, email and password are invalids", async () => {
+  it("POST /auth/register -> 400 error, body parameters are invalids", async () => {
     const response = await request(app)
-      .post("/api/auth/register")
+      .post(endpoint)
       .expect("Content-Type", /json/)
       .expect(400);
     expect(response.body).toEqual(
@@ -51,22 +55,73 @@ describe.skip("Register Part", () => {
   });
 });
 
-describe("Login Part", () => {
+describe.skip("Login", () => {
+  const endpoint = "/api/auth/login";
   it("POST /auth/login -> 200, tokens in response", async () => {
     const data = {
       email: "email@mail.ru",
       password: "123456",
     };
     const response = await request(app)
-      .post("/api/auth/login")
+      .post(endpoint)
       .send(data)
       .expect("Content-Type", /json/)
       .expect(200);
+
+    accessToken = response.body.accessToken;
+    refreshToken = response.body.refreshToken;
+
     expect(response.body).toEqual(
       expect.objectContaining({
         accessToken: expect.any(String),
         refreshToken: expect.any(String),
       })
     );
+  });
+  it("POST /auth/login -> 401, email or password are invalids", async () => {
+    const data = {
+      email: "email@mail.ru",
+      password: "123456789",
+    };
+    const response = await request(app)
+      .post(endpoint)
+      .expect("Content-Type", /json/)
+      .send(data)
+      .expect(401);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: expect.any(String),
+      })
+    );
+  });
+  it("POST /auth/login -> 400, body parameters are invalids", async () => {
+    const response = await request(app)
+      .post(endpoint)
+      .expect("Content-Type", /json/)
+      .expect(400);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        errors: expect.arrayContaining([
+          expect.objectContaining({
+            msg: expect.any(String),
+            param: expect.any(String),
+            location: expect.any(String),
+          }),
+        ]),
+      })
+    );
+  });
+});
+
+describe.skip("Logout", () => {
+  const endpoint = "/api/auth/logout";
+  it("DELETE /auth/logout -> 200 logout user with accessToken", async () => {
+    await request(app)
+      .delete(endpoint)
+      .set({ Authorization: accessToken })
+      .expect(200);
+  });
+  it("DELETE /auth/logout -> 403 access is denied", async () => {
+    await request(app).delete(endpoint).expect(403);
   });
 });
